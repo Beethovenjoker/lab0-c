4e975dfc13567f4413be25280c0c1e47c2c37876
Beethovenjoker <c11544211690@gmail.com>
2025-02-27 14:17:15 +0800

    Implement queue remove head and queue remove tail
    First, check if the head node exists.
    Then, assign the node we want to remove.
    And then, copy the value to the string.
    Finally, remove the node and return it.
    
    Change-Id: I97379500f150ed9cc732423f7f11415370bab6ff

--- queue.c
+++ queue.c
@@ -58,21 +58,33 @@ bool q_insert_head(struct list_head *head, char *s)
 /* Insert an element at tail of queue */
 bool q_insert_tail(struct list_head *head, char *s)
 {
-    if (!head)
-        return false;
     return q_insert_head(head->prev, s);
 }
 
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head)
+        return NULL;
+
+    element_t *node = list_entry(head->next, element_t, list);
+    strncpy(sp, node->value, bufsize);
+    list_del(&node->list);
+
+    return node;
 }
 
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head)
+        return NULL;
+
+    element_t *node = list_entry(head->prev, element_t, list);
+    strncpy(sp, node->value, bufsize);
+    list_del(&node->list);
+
+    return node;
 }
 
 /* Return number of elements in queue */
